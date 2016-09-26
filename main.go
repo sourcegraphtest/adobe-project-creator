a605a3e859bd8babe9eb4b83a1bcb989289f8ea6
jspc <james@zero-internet.org.uk>
2016-09-26 14:28:28 +0100

    Optional rabbitmq as a delivery mechanism

--- main.go
+++ main.go
@@ -5,22 +5,35 @@ import (
     "log"
     "net/http"
     "os"
-    "os/signal"
-    "syscall"
 )
 
+var amqpUri *string
 var cluster Cluster
+var mode *string
 
 func LoadConfig(){
     cluster.ID = os.Getenv("CLUSTER_ID")
 }
 
 func main(){
+    amqpUri = flag.String("amqp-uri", "amqp://guest:guest@localhost:5671/vhost", "amqp uri when in rabbit mode")
+    mode = flag.String("mode", "rest", "Mode in which to listen [rest|rabbit]")
     flag.Parse()
 
     LoadConfig()
     log.Printf( "Returning creating projects under the %s cluster", cluster.ID )
 
-    http.HandleFunc("/", Router)
-    http.ListenAndServe(":8000", nil)
+    switch *mode {
+    case "rest":
+        http.HandleFunc("/", Router)
+        http.ListenAndServe(":8000", nil)
+    case "rabbit":
+        //
+        _, err := NewConsumer(*amqpUri, "something", "somethingelse")
+        if err != nil {
+            log.Fatal(err.Error())
+        }
+    default:
+        log.Fatalf("Mode %s is invalid", mode)
+    }
 }
