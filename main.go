d9b80dd98bbacc1baf191d3372aa0a2239bcb115
jspc <james@zero-internet.org.uk>
2016-09-27 10:57:20 +0100

    Handle initialisation in the startup phase

--- main.go
+++ main.go
@@ -11,16 +11,19 @@ var amqpUri *string
 var cluster Cluster
 var mode *string
 
-func LoadConfig(){
+func init(){
     cluster.ID = os.Getenv("CLUSTER_ID")
-}
 
-func main(){
     amqpUri = flag.String("amqp-uri", "amqp://guest:guest@localhost:5671/vhost", "amqp uri when in rabbit mode")
     mode = flag.String("mode", "rest", "Mode in which to listen [rest|rabbit]")
     flag.Parse()
 
-    LoadConfig()
+    if cluster.ID == "" {
+        log.Fatal("No flex cluster defined, please specify the environment variable 'CLUSTER_ID'")
+    }
+}
+
+func main(){
     log.Printf( "Creating projects under the %s cluster", cluster.ID )
     log.Printf( "Running in %s mode", *mode )
 
